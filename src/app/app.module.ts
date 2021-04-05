3f8a9fc67d3595894aa6dce78811e9405e111160
Kaueslo <kaue.sloboda@hotmail.com>
2021-04-05 20:35:35 -0300

    Criando o módulo Calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//Por termos feito o index.ts na pasta calculadora, ele consegue fazer o import da forma abaixo
+//De uma certa forma, a classe fica pública
+import { CalculadoraModule } from './calculadora';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalculadoraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
