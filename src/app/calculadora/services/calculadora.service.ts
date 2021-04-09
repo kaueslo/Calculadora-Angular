0739d63a823061897b0eb4a245c1483b8f0cc551
Kaueslo <kaue.sloboda@hotmail.com>
2021-04-08 22:01:08 -0300

    Realizando as operações matemáticas do service calculadora

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -7,5 +7,41 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  /*Define as constante utilizadas
+    para identificar as operações de cálculo */
+    static readonly SOMA: string = '+';
+    static readonly SUBTRACAO: string = '-';
+    static readonly DIVISAO: string = '/';
+    static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+
+  /*Método que calcula uma operação matemáica dado
+  dois números. */
+
+  calcular(num1: number, num2: number, operacao: string): number{
+    let resultado: number; //Armazena o resultado da operação
+
+    switch(operacao){
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+      break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+      break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+      break;
+      case CalculadoraService.DIVISAO:
+        if(num2 == 0){
+          alert('Não é possível dividir por 0');
+          break
+        }
+        resultado = num1 / num2;
+      break;
+      default:
+        resultado = 0;
+    }
+    return resultado;
+  }
 }
